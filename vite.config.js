1ab693a9e07fc1f8e6e2173d463bf546d63fdbd1
Fabian <fabian.posso@gmail.com>
2025-06-05 01:17:46 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/taller5-innovaccion/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['tienda3d.loca.lt'], // 👈 aquí permites el túnel
+    allowedHosts: ['tienda3d.loca.lt'],
   },
 })
