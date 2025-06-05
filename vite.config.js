294b83d3cc2ef9774b94b25f0733d76330bed5e0
Fabian <fabian.posso@gmail.com>
2025-06-05 01:45:51 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/taller5-innovaccion/',
+export default defineConfig({
+  base: '/', 
   plugins: [react()],
-}));
+});
